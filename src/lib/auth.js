243c02775404f4ea77cbfe7430e5a81e252b8508
Gagi Shmagi <gagi.shmagi@gmail.com>
2021-09-17 12:41:22 +0300

    Issue with auth on refresh

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -25,69 +25,14 @@ class Auth{
             cb()
         })
 
-        // signOut(auth).then(()=>{
-        //     this.authenticated = false
-        //     cb()     
-        // })
     }
 
     isAuthenticated(){
 
-        // let user = firebase.auth().currentUser;
-
-        // console.log(user)
-
-        // return (user !== null) ? this.authenticated = true : this.authenticated = false ; 
-
-        const authPromise = new Promise((resolve, reject) => {
-             auth.onAuthStateChanged((user) => {
-
-                if (user) {
-                
-                  var uid = user.uid;
-                
-                  resolve(user)
-                    
-                } else {
-                
-                  reject()
-                
-                }
-                
-            })
-            
-        })
-        
-        // console.log(auth.currentUser)
-        
-        return authPromise.then((user) => {
-            // if(this._saveUserDataToLocalStorage(user) === 'success')
-            console.log(user)
-
-            this.authenticated = true
-            
-            
-        }).catch((res) => {
-            this.authenticated = false
-            
-            // console.log("some errror", e)
-        });
-        
         return this.authenticated
-
-
 
     }
 
-    // _saveUserDataToLocalStorage(user){
-    //     const {email, displayName } = user
-
-    //     const newUser = {
-    //         email, displayName 
-    //     }
-    //     localStorage.setItem("user", JSON.stringify(newUser))
-    //     return 'success'
-    // }
 }
 
 export default new Auth()
